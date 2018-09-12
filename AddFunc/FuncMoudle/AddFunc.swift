39b67eeef0e69983ce573b313e8fb5ff2e2fbb23
larry <lxd_super_main@126.com>
2018-09-12 18:54:09 +0800

    Add func

---
+++ AddFunc/FuncMoudle/AddFunc.swift
@@ -0,0 +1,15 @@
+//
+//  AddFunc.swift
+//  AddFunc
+//
+//  Created by larry on 2018/9/12.
+//  Copyright © 2018年 twofly. All rights reserved.
+//
+
+import Foundation
+
+extension NSObject {
+    func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
---
+++ AddFunc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AddFunc
+//
+//  Created by larry on 2018/9/12.
+//  Copyright © 2018年 twofly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let c = add(a: 4, b: 6)
+        print(c)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
